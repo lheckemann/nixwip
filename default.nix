8ae1815bef45dc2f6a91936141b93f00e07215ae
Linus Heckemann <git@sphalerite.org>
2018-11-16 17:09:32 +0100

    manaplus: init at 1.9.8.1

--- default.nix
+++ default.nix
@@ -8,4 +8,18 @@
     }} $out/bin/pb
     runHook fixupPhase
   '';
+
+  manaplus = with pkgs; stdenv.mkDerivation rec {
+    name = "${pname}-${version}";
+    pname = "manaplus";
+    version = "1.8.9.1";
+    nativeBuildInputs = [autoreconfHook pkgconfig];
+    buildInputs = [SDL SDL_mixer SDL_image SDL_net SDL_ttf SDL_gfx libxml2 zlib curl libGL libpng];
+    src = fetchFromGitLab {
+      owner = pname;
+      repo = pname;
+      rev = "v${version}";
+      sha256 = "1ni4qhhcyaixf5nhlwh0q5khsl76wpc4c0yflmvxlmf28wyy60lg";
+    };
+  };
 }
