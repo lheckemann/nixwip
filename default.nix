a2a0916234568b3880b06bffc336a1ebc104d276
Linus Heckemann <git@sphalerite.org>
2020-12-02 22:35:05 +0100

    fix eval?

--- default.nix
+++ default.nix
@@ -2,7 +2,7 @@
   asql = pkgs.callPackage ./asql.nix {};
   x11 = pkgs.recurseIntoAttrs (pkgs.callPackages ./x11 {});
   pb_cli = pkgs.runCommand "pb" {} ''
-    install -Dm 555 ${builtins.fetchurl {
+    install -Dm 555 ${pkgs.fetchurl {
       url = https://raw.githubusercontent.com/ptpb/pb_cli/5242382b3d6b5c0ddaf6e4843a69746b40866e57/src/pb.sh;
       sha256 = "146cfsy3mxlzkx8m20fqzrswvgkj2vfgfqrjd0kl6lca33iw758m";
     }} $out/bin/pb
